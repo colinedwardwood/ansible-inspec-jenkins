5142214629753178353c8a2c38912246fb3c78c3
cewdesign <woodce@gmail.com>
2020-02-26 11:07:30 -0500

    updated tests to verify httpd setup

--- test/integration/default/site_verify.rb
+++ test/integration/default/site_verify.rb
@@ -1,3 +1,20 @@
-describe ssl(port: 443).protocols('ssl3') do
-  it { should_not be_enabled }
+# check for hello world file
+describe file('/tmp/testfile.txt') do
+  its('content') { should match('hello world')}
+end
+
+# check httpd package is installed
+describe package 'httpd' do
+  it { should be_installed }
+end
+
+# check httpd service is running and enabled
+describe service 'httpd' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+# check httpd is listening on port 80
+describe port 80 do
+  it { should be_listening }
 end
